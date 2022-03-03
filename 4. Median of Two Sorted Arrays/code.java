047a7138a6ba6e2aca41643cef600420a191ad6c
Manish <manishprajapatim416@gmail.com>
2022-03-03 17:37:35 +0530

    4. Median of Two Sorted Arrays

---
+++ 4. Median of Two Sorted Arrays/code.java
@@ -0,0 +1,12 @@
+public class Solution {
+    // you need treat n as an unsigned value
+    public int reverseBits(int n) {
+        int m=0;
+        for(int i=1;i<=32;i++)
+        {
+            m=(m<<1)|(n&1);
+            n>>=1;
+        }
+        return m;
+    }
+}
