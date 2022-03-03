b75ead3b1394896e59c13994bce1e0afedf6665e
Manish <manishprajapatim416@gmail.com>
2022-03-03 17:38:00 +0530

    .

--- 4. Median of Two Sorted Arrays/code.java
+++ 4. Median of Two Sorted Arrays/code.java
@@ -1,12 +1,31 @@
-public class Solution {
-    // you need treat n as an unsigned value
-    public int reverseBits(int n) {
-        int m=0;
-        for(int i=1;i<=32;i++)
-        {
-            m=(m<<1)|(n&1);
-            n>>=1;
+class Solution {
+    public double findMedianSortedArrays(int[] nums1, int[] nums2) {
+        int n1= nums1.length; 
+        int n2= nums2.length;
+        
+        int nums[]= new int[n1+n2];
+        
+        int i=0, j=0, k=0;
+        
+        while(i<n1 && j<n2){
+            if(nums1[i] < nums2[j])
+                nums[k++]=nums1[i++];
+            else
+                nums[k++]=nums2[j++];
         }
-        return m;
+        
+        while(i<n1)
+            nums[k++]=nums1[i++];
+        
+        while(j<n2)
+            nums[k++]=nums2[j++];
+        
+        double median=0;
+        if(nums.length % 2 == 0)
+        median = (double) (nums[nums.length / 2] + nums[(nums.length / 2) - 1])/2;   
+        else 
+        median = (double) nums[nums.length / 2];
+     
+        return median;    
     }
 }
