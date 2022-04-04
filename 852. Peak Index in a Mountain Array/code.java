8f8b94a176efe876064397ded716b57c824ab50c
Manish <manishprajapatim416@gmail.com>
2022-04-04 17:06:03 +0530

    852. Peak Index in a Mountain Array

--- 852. Peak Index in a Mountain Array/code.java
+++ 852. Peak Index in a Mountain Array/code.java
@@ -13,3 +13,23 @@ class Solution {
         return e;
     }
 }
+
+
+
+// AGAIN
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int n = arr.length;
+        int l = 0;
+        int h = n-1;
+        while(l<h){
+            int m = l + (h-l)/2;
+            if(arr[m]<arr[m+1]){
+                l=m+1;
+            }else{
+                h=m;
+            }
+        }
+        return l;
+    }
+}
