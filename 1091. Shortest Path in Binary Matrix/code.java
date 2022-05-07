89610d2709fbad0ddcc4f0b47a5b1f8a9a121b66
Manish <manishprajapatim416@gmail.com>
2022-05-07 16:59:32 +0530

    1091. Shortest Path in Binary Matrix

--- 1091. Shortest Path in Binary Matrix/code.java
+++ 1091. Shortest Path in Binary Matrix/code.java
@@ -1,34 +1,39 @@
 class Solution {
     private static final int[][] DIRS = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}, {-1, 1}, {-1, -1}, {1, 1}, {1, -1}};
-
+    
     public int shortestPathBinaryMatrix(int[][] grid) {
-        if (grid == null || grid.length == 0 || grid[0].length == 0) return 0;
-        final int M = grid.length, N = grid[0].length;
-        if (grid[0][0] == 1 || grid[M-1][N-1] == 1) return -1;
-        if (M == 1 && N == 1) return 1;
+        int n = grid.length,m = grid[0].length;
+        if(grid[0][0]==1 || grid[n-1][m-1]==1) return -1;
+        if(n==1 && m==1) return 1;
+        
         int depth = 1;
-        Queue<int[]> que = new ArrayDeque<>();
-        que.add(new int[]{0, 0});
+        Queue<int[]> queue = new LinkedList<>();
+        queue.add(new int[]{0,0});
         grid[0][0] = -1;
-        while (!que.isEmpty()) {
+        
+        while(!queue.isEmpty()){
             depth++;
-            int size = que.size();
-            for (int k = 0; k < size; k++) {
-                int[] cell = que.poll();
-                for (int[] dir : DIRS) {
+            int size = queue.size();
+            while(size-- > 0){
+                int[] cell = queue.poll();
+                for(int[] dir : DIRS){
                     int x = cell[0] + dir[0];
                     int y = cell[1] + dir[1];
-                    if (outOfBound(grid, x, y) || grid[x][y] != 0) continue;
-                    if (x == M-1 && y == N-1) return depth;
-                    que.add(new int[]{x, y});
-                    grid[x][y] = -1;
+                    if(isValid(x,y,n,m) || grid[x][y]!=0) continue;
+                    if(x==n-1 && y==m-1) return depth;
+                    queue.add(new int[]{x,y});
+                    grid[x][y]=-1;
                 }
             }
         }
+        
+        
+        
         return -1;
     }
     
-    private static boolean outOfBound(int[][] grid, int x, int y) {
-        return x < 0 || y < 0 || x >= grid.length || y >= grid[0].length;
+    boolean isValid(int i,int j,int n,int m){
+        return i<0||j<0||i>=n||j>=m;
     }
+    
 }
